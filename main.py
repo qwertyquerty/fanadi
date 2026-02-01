f6e5ff243f72e631c73a09df566a5e7fd54cff72
madeline <qwertytrogi@gmail.com>
2026-02-01 00:32:04 -0800

    buid

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+if sys.stdout is None:
+    sys.stdout = open(os.devnull, "w")
+if sys.stderr is None:
+    sys.stderr = open(os.devnull, "w")
+
 from fanadi import *
 
 app = QApplication(sys.argv)
